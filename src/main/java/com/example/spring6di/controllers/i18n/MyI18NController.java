0c2ec95193947ab5791513d690b17523b3d94848
Lowe0 <45861748+Lowe0@users.noreply.github.com>
2024-03-15 14:48:20 -0400

    setup English profile

--- src/main/java/com/example/spring6di/controllers/i18n/MyI18NController.java
+++ src/main/java/com/example/spring6di/controllers/i18n/MyI18NController.java
@@ -1,13 +1,14 @@
 package com.example.spring6di.controllers.i18n;
 
 import com.example.spring6di.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyI18NController {
     private final GreetingService greetingService;
 
-    public MyI18NController(GreetingService greetingService) {
+    public MyI18NController(@Qualifier("i18NService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
--- src/main/java/com/example/spring6di/services/i18n/EnglishGreetingService.java
+++ src/main/java/com/example/spring6di/services/i18n/EnglishGreetingService.java
@@ -3,7 +3,7 @@ package com.example.spring6di.services.i18n;
 import com.example.spring6di.services.GreetingService;
 import org.springframework.stereotype.Service;
 
-@Service
+@Service("i18NService")
 public class EnglishGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
