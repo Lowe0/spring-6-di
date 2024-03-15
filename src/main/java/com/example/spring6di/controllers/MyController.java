4f37e7df40759c36bcd51756a27ac93338517dca
Lowe0 <45861748+Lowe0@users.noreply.github.com>
2024-03-15 13:37:30 -0400

    add service dependency (per course lecture, intentionally done wrong)

--- src/main/java/com/example/spring6di/controllers/MyController.java
+++ src/main/java/com/example/spring6di/controllers/MyController.java
@@ -1,11 +1,20 @@
 package com.example.spring6di.controllers;
 
+import com.example.spring6di.services.GreetingService;
+import com.example.spring6di.services.GreetingServiceImpl;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        //this.greetingService = greetingService;
+        this.greetingService = new GreetingServiceImpl();
+    }
+
     public String sayHello() {
         System.out.println("In the controller");
-        return "Hello from the controller.";
+        return greetingService.sayGreeting();
     }
 }
